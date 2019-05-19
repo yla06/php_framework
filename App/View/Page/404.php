66533f616f86a2402421951cf1e0d197b22338ce
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 19:02:25 +0300

    Create 404.php

---
+++ App/View/Page/404.php
@@ -0,0 +1,16 @@
+<style>
+    h1 {
+      text-align: center;
+      padding-top: 50px;
+      color: lightblue;
+    }
+    div {
+        height: 500px;
+        width: 500px;
+        background: url('/App/View/Page/404.ipg');
+    }
+</style>
+<h1>Извините страница не найдена или была удалена</h1>
+<div></div>
+    <!--<img src="image/404.ipg">-->
+    

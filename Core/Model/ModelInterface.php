b0a5c2e6509cb5c2d3b459b4cf608c8d7b45c6af
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 19:10:29 +0300

    Create ModelInterface.php

---
+++ Core/Model/ModelInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace Model;
+
+interface ModelInterface
+{
+    public function save();
+    public function insert();
+    public function update();
+    public function delete();
+    
+    
+ 
+    
+}

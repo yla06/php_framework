96291e9b17548b8c96951e764688bd67d1c77288
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 18:54:16 +0300

    Create Global.php

---
+++ App/Config/Global.php
@@ -0,0 +1,10 @@
+<?php
+require_once EVRIKA_ROOT . '/App/Config/Database.php';
+require_once EVRIKA_ROOT . '/App/Config/Router.php';
+
+date_default_timezone_set( 'Etc/GMT+0' );
+
+//\Route\Router::setRule( '/about', ['About', 'Action'] );
+//\Route\Router::setRule( '/contact', ['Static', 'Contact'] );
+
+

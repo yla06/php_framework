1db7390f33388d9adb177890f4bdcc015a2a3e06
yla06 <33964687+yla06@users.noreply.github.com>
2019-05-19 19:01:10 +0300

    Create Contact.php

---
+++ App/View/Page/Static/Contact.php
@@ -0,0 +1,7 @@
+<?php self::startBlock( 'title' ) ?>Title<?php self::endBlock( 'title' )?>
+
+<?php self::startBlock( 'content' ) ?>
+<h1>this is contact page <?= $foo ?></h1>
+<?php self::endBlock( 'content' )?>
+
+<?php self::setLayout( 'Admin' ) ?>
